fe5590d7a6ee4c69f2ad9596b2f814a75cc29f6d
zziwonCHOI <wldnjssem6@naver.com>
2024-06-28 17:09:16 +0900

    feat: navbar

---
+++ homepage/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Logo from '../assets/images/logo.png'
+import '../styles/header.scss'
+import {Link} from 'react-router-dom'
+const Header = () => {
+    return (
+        <div className='container'>
+            <a href='/'><img src={Logo} alt='Logo'></img></a>
+            <div>
+            <Link className='navMenu' to={'/about'}>About</Link>
+            <Link className='navMenu' to={'/study'}>Study</Link>
+            <Link className='navMenu' to={'/project'}>Project</Link>
+            </div>
+            <div className='apply'>
+                <p>지원하기</p>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ homepage/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+import {Link} from 'react-router-dom'
+import Nav from '../navigation/Nav';
+
+const Navbar = () => {
+    return (
+        <Header/>
+    );
+};
+
+export default Navbar;
