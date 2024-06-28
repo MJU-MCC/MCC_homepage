0cafa4b9cd98ae96d21122592daa0d1d16248458
zziwonCHOI <wldnjssem6@naver.com>
2024-06-28 17:04:49 +0900

    feat:초기화면 및 routing

--- homepage/src/App.js
+++ homepage/src/App.js
@@ -1,15 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 import Main from './components/Main';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Nav from './navigation/Nav';
+import About from './components/About';
+import Study from './components/Study';
+import Project from './components/Project';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
 
-
-function App() {
+const App=()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Main></Main>
-      </header>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Navbar/>
+          <Routes>
+            <Route path='/' element={<Main/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/study' element={<Study/>}/>
+            <Route path='/project' element={<Project/>}/>
+          </Routes>
+        </header>
     </div>
+    </Router>
+
   );
 }
 
