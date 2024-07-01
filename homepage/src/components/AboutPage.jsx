f1668ea268932f9464085a9b685190edeeeda8a7
zziwonCHOI <wldnjssem6@naver.com>
2024-07-01 20:54:50 +0900

    chore: 운영진 소개 추가

--- homepage/src/components/AboutPage.jsx
+++ homepage/src/components/AboutPage.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../styles/aboutPage.scss'
 import Jiwon from '../assets/images/jiwon.PNG'
 import Sh from '../assets/images/sh.PNG'
 import Coco from '../assets/images/coconut.PNG'
 import Iy from '../assets/images/iy.PNG'
+import Seh from '../assets/images/seh.PNG'
+import Hs from '../assets/images/hs.PNG'
+import Yhs from '../assets/images/yhs.PNG'
+import Jw from '../assets/images/jw.PNG'
 const AboutPage = () => {
     return (
         <div className='abouts_container'>
@@ -25,10 +29,10 @@ const AboutPage = () => {
                     M.C.C 자체 워크북을 이용해서 체계적으로 관리</p>
             </div>
             <div className='what'>
-                <h2>what</h2>
-                <p>기초부터 차근차근 공부하는 study<br/>
-                기획부터 배포까지 경험 가능한 project<br/>
-                다양한 사람들과의 communication
+                <h2>why</h2>
+                <p>유일한 명지대 컴퓨터 관련 중앙동아리!<br/>
+                다양한 과에 속한 사람들과의 networking<br/>
+                부담없이 내가 원하는 분야를 공부
                 </p>
             </div>
             <div className='management'>
@@ -36,7 +40,7 @@ const AboutPage = () => {
                 <p>45기 운영진</p>
                 <div className='flex-item'>
                     <div className='item'>
-                        <img src={Jiwon} alt='jiwon'></img>
+                        <img src={Hs} alt='hs'></img>
                         <p id='role'>회장</p>
                         <p id='name'>김현성</p>
                         <p id='role'>명지대학교 컴퓨터공학과</p>
@@ -68,17 +72,24 @@ const AboutPage = () => {
                         <p id='role'>명지대학교 정보통신공학과</p>
                     </div>
                     <div className='grid-item'>
-                        <img src={Iy} alt='iy'></img>
-                        <p id='role'>기록부</p>
-                        <p id='name'>윤현선</p>
-                        <p id='role'>명지대학교 컴퓨터공학과</p>
-                    </div>
-                    <div className='grid-item'>
-                        <img src={Iy} alt='iy'></img>
+                        <img src={Seh} alt='seh'></img>
                         <p id='role'>디자인부</p>
                         <p id='name'>방세현</p>
                         <p id='role'>명지대학교 정보통신공학과</p>
                     </div>
+                    <div className='grid-item'>
+                        <img src={Yhs} alt='hy'></img>
+                        <p id='role'>사무부</p>
+                        <p id='name'>윤현선</p>
+                        <p id='role'>명지대학교 컴퓨터공학과</p>
+                    </div>
+                    <div className='grid-item'>
+                        <img src={Jw} alt='jw'></img>
+                        <p id='role'>차기회장</p>
+                        <p id='name'>이정우</p>
+                        <p id='role'>명지대학교 전자공학과</p>
+                    </div>
+                    
                 </div>
             </div>
         </div>
