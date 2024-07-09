dd885c286565fb5e2331f074c5e8825c771533c6
zziwonCHOI <wldnjssem6@naver.com>
2024-07-09 20:38:14 +0900

    fix: 화면 전환시 스크롤 새로고침 안되는 부분 해결

--- homepage/src/App.js
+++ homepage/src/App.js
@@ -1,18 +1,28 @@
 import './App.css';
 import Main from './components/Main';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation} from 'react-router-dom';
 import About from './components/About';
 import Study from './components/Study';
-import Project from './components/Project';
+import ProjectPage from './components/ProjectPage';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import AboutPage from './components/AboutPage';
 import StudyPage from './components/StudyPage';
 import Footer from './components/Footer';
+import { useEffect } from 'react';
+
+
+export  function ScrollToTop(){
+  const {pathname}=useLocation();
+  useEffect(()=>{
+    window.scrollTo(0,0)
+  },[pathname])
+  return null
+}
 
 const App=()=> {
+
   return (
-    <Router>
       <div className="App">
           <Navbar/>
           <div className="App-header">
@@ -20,12 +30,11 @@ const App=()=> {
             <Route path='/' element={<Main/>}/>
             <Route path='/about' element={<AboutPage/>}/>
             <Route path='/study' element={<StudyPage/>}/>
-            <Route path='/project' element={<Project/>}/>
+            <Route path='/project' element={<ProjectPage/>}/>
           </Routes>
           </div>
           <Footer/>
     </div>
-    </Router>
 
   );
 }
